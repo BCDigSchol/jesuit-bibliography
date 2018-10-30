18f46f796e727cd4170f2d72d33a47c7b083b27b
Jesse Martinez <mjesse@gmail.com>
2018-10-30 10:39:25 -0400

    add missing relationship options
    make sure associated records are destroyed when parent records is deleted

--- app/models/bibliography.rb
+++ app/models/bibliography.rb
@@ -1,5 +1,5 @@
 class Bibliography < ApplicationRecord
-    has_many :comments, dependent: :destroy
+    has_many :comments, inverse_of: :bibliography, dependent: :destroy
 
     # one-to-many relationship using class_name and foreign_key atributes
     has_many :isbns, class_name: 'StandardIdentifier', foreign_key: 'isbn_id', inverse_of: 'isbn', dependent: :destroy
@@ -7,11 +7,11 @@ class Bibliography < ApplicationRecord
     has_many :dois,  class_name: 'StandardIdentifier', foreign_key: 'doi_id',  inverse_of: 'doi', dependent: :destroy
 
     # many-to-many relationship through bibliography_subjects
-    has_many :bibliography_subjects, inverse_of: :bibliography
+    has_many :bibliography_subjects, inverse_of: :bibliography, dependent: :destroy
     has_many :subjects, through: :bibliography_subjects
 
     # many-to-many relationship through bibliography_periods
-    has_many :bibliography_periods, inverse_of: :bibliography
+    has_many :bibliography_periods, inverse_of: :bibliography, dependent: :destroy
     has_many :periods, through: :bibliography_periods
 
     accepts_nested_attributes_for :comments, allow_destroy: true, reject_if: ->(comments){ comments['body'].blank? }
