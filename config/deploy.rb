66873aa9664f2cfc090af40ae4812043fcac1ffa
Benjamin Florin <benjamin.florin@gmail.com>
2018-11-09 16:30:35 -0500

    Save secrets as ENV variables.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,27 +1,32 @@
 # Change these
-server 'server', port: 7171, roles: [:web, :app, :db], primary: true
+server 'lutro.bc.edu', roles: [:web, :app, :db], primary: true
 
-set :repo_url,        'git@github.com:BCDigSchol/jesuit-bibliography.git'
-set :application,     'bc-jesuit-bibliography'
-set :user,            'blacklight'
-set :puma_threads,    [4, 16]
-set :puma_workers,    0
+set :repo_url, 'git@github.com:BCDigSchol/jesuit-bibliography.git'
+set :application, 'bc-jesuit-bibliography'
+set :user, 'blacklight'
+set :puma_threads, [4, 16]
+set :puma_workers, 0
 
 # Don't change these unless you know what you're doing
-set :pty,             true
-set :use_sudo,        false
-set :stage,           :production
-set :deploy_via,      :remote_cache
-set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
-set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
-set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :pty, true
+set :use_sudo, false
+set :branch, ENV['BRANCH'] if ENV['BRANCH']
+set :stage, :production
+set :deploy_via, :remote_cache
+set :deploy_to, "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind, "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state, "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid, "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
-set :puma_error_log,  "#{release_path}/log/puma.access.log"
-set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_error_log, "#{release_path}/log/puma.access.log"
+set :ssh_options, {forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub)}
 set :puma_preload_app, true
 set :puma_worker_timeout, nil
-set :puma_init_active_record, false  # Change to true if using ActiveRecord
+set :puma_init_active_record, false # Change to true if using ActiveRecord
+# set :branch, "move-production-to-puma"
+
+set :rbenv_type, :user # or :system, depends on your rbenv setup
+set :rbenv_ruby, '2.5.1'
 
 ## Defaults:
 # set :scm,           :git
@@ -53,7 +58,6 @@ namespace :deploy do
       unless `git rev-parse HEAD` == `git rev-parse origin/master`
         puts "WARNING: HEAD is not the same as origin/master"
         puts "Run `git push` to sync changes."
-        exit
       end
     end
   end
@@ -73,10 +77,10 @@ namespace :deploy do
     end
   end
 
-  before :starting,     :check_revision
-  after  :finishing,    :compile_assets
-  after  :finishing,    :cleanup
-  after  :finishing,    :restart
+  before :starting, :check_revision
+  after :finishing, :compile_assets
+  after :finishing, :cleanup
+  after :finishing, :restart
 end
 
 # ps aux | grep puma    # Get puma pid
