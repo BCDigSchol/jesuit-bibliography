fdf5a8d189c3645695010a20f038ec8f72f20b95
Benjamin Florin <benjamin.florin@gmail.com>
2018-11-29 16:47:19 -0500

    For debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
+  config.assets.debug = true
+  config.assets.resolve_with = %i[manifest]
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
