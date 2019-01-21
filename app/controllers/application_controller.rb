5c4b7c23cf610b6d0f55a6b22d4a068509b51493
jessemartinez <mjesse@gmail.com>
2019-01-20 19:22:06 -0500

    manage redirect back to current page for sign-ins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,22 @@
 class ApplicationController < ActionController::Base
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
+
+  before_action :store_user_location!, if: :storable_location?
+  # The callback which stores the current location must be added before you authenticate the user 
+  # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
+  # before the location can be stored.
+  before_action :authenticate_user!
+
   layout 'blacklight'
 
-  def after_sign_in_path_for(resource)
-    dashboard_path
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || super
   end
 
   def require_login
     unless current_user
-        redirect_to dashboard_path
+        redirect_to user_session_path
     end
   end
 
@@ -17,4 +24,20 @@ class ApplicationController < ActionController::Base
     #redirect_to dashboard_url, :alert => exception.message
     render template: "bibliographies/_not_permitted", locals: {alert: exception.message}
   end
+
+  private
+    # https://github.com/plataformatec/devise/wiki/How-To:-Redirect-back-to-current-page-after-sign-in,-sign-out,-sign-up,-update
+    # Its important that the location is NOT stored if:
+    # - The request method is not GET (non idempotent)
+    # - The request is handled by a Devise controller such as Devise::SessionsController as that could cause an 
+    #    infinite redirect loop.
+    # - The request is an Ajax request as this can lead to very unexpected behaviour.
+    def storable_location?
+      request.get? && is_navigational_format? && !devise_controller? && !request.xhr? 
+    end
+
+    def store_user_location!
+      # :user is the scope we are authenticating
+      store_location_for(:user, request.fullpath)
+    end
 end
