6ad87ffb662e06898549d1470be8bf24784ca646
Jesse Martinez <mjesse@gmail.com>
2019-01-22 11:57:24 -0500

    prevent authentication on public BL pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,9 @@ class ApplicationController < ActionController::Base
   # The callback which stores the current location must be added before you authenticate the user 
   # as `authenticate_user!` (or whatever your resource is) will halt the filter chain and redirect 
   # before the location can be stored.
-  before_action :authenticate_user!
+  # 
+  # We already add this filter in the specific controller actions we need for authentication
+  #before_action :authenticate_user!
 
   layout 'blacklight'
 
