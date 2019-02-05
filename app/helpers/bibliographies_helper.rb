e8700263653fd07b6d105ea256c0678895f3dcde
jessemartinez <mjesse@gmail.com>
2019-02-04 22:53:32 -0500

    updating how dates are displayed in backend
    following ISO 8601 international date standard with some minor modifications to timezone for readability

--- app/helpers/bibliographies_helper.rb
+++ app/helpers/bibliographies_helper.rb
@@ -2,4 +2,9 @@ module BibliographiesHelper
     def creator_match?(current_user, bib_creator)
         current_user.to_s == bib_creator.to_s
     end
+
+    def format_date_string(date)
+        # TODO make sure this is a date-type object
+        date.in_time_zone('Eastern Time (US & Canada)').strftime "%Y-%m-%d %H:%M %Z"
+    end
 end
